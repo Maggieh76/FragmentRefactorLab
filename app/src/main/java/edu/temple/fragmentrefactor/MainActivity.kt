a9dd25286ac618aec80920d311bc9df5bb07ddc6
Margaret Hanley <tuj92824@temple.edu>
2023-10-23 15:49:31 -0400

    use of fragment manager

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //supportFragmentManager.beginTransaction().add(R.id.container, NameFragment()).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, NameFragment()).commit()
     }
 }
